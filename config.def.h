c9116a8273f7c6c79f6bae4aa44018a614ce0079
Vlad K <vlad@void-vm.mshome.net>
2019-06-13 13:44:55 -0400

    Fixing font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=26:antialias=true:autohint=true:dpi=220";
 static int borderpx = 2;
 
 /*
